55ef0c308ffebcf7428e7b3c400375920f93e57c
Max Heisinger <mail@maximaximal.com>
2019-01-07 03:49:24 +0100

    Add workaround for shared linking with RCore and faster build.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ chmod +x ./rtest-amd64
 ./rtest-amd64 cmake -BbuildLinuxAmd64 -H. -G"Unix Makefiles" -DBUILD_NUMBER=$BUILD_NUMBER -DBUILD_URL=$BUILD_URL -DCMAKE_BUILD_TYPE=Release
 
 # Build
-./rtest-amd64 make -C buildLinuxAmd64 -j 2
+./rtest-amd64 make -C buildLinuxAmd64 -j 4
 
 # Package
-./rtest-amd64 make -C buildLinuxAmd64 -j 2 package
+./rtest-amd64 make -C buildLinuxAmd64 -j 4 package
